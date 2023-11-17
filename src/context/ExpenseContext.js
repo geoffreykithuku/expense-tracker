099c482cfe4098ad6ca026caf85401c0d9e068c5
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-11-17 12:06:48 +0300

    feat: persist data to local storage

--- src/context/ExpenseContext.js
+++ src/context/ExpenseContext.js
@@ -1,15 +1,22 @@
-import { createContext, useState } from "react";
-
-const initialTransactions = [
-];
-
+import { createContext, useEffect, useState } from "react";
 
 export const ExpenseContext = createContext();
 
 export const ExpenseProvider = ({ children }) => {
-  const [transactions, setTransactions] = useState(initialTransactions);
+  const [transactions, setTransactions] = useState([]);
   const [type, setType] = useState("all");
 
+  //persist data in local storage
+  useEffect(() => {
+    const localTransactions = localStorage.getItem("transactions");
+
+    setTransactions(JSON.parse(localTransactions) || []);
+  }, []);
+
+  //save data in local storage
+  useEffect(() => {
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+  }, [transactions]);
 
   const addTransaction = (transaction) => {
     setTransactions([...transactions, transaction]);
@@ -19,7 +26,7 @@ export const ExpenseProvider = ({ children }) => {
     const newTransactions = transactions.filter(
       (transaction) => transaction.id !== id
     );
-      
+
     setTransactions(newTransactions);
   };
 
@@ -27,6 +34,7 @@ export const ExpenseProvider = ({ children }) => {
   const incomeAmounts = transactions
     .filter((transaction) => transaction.category === "income")
     .map((transaction) => transaction.amount);
+
   const income =
     incomeAmounts.length > 0
       ? incomeAmounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
@@ -36,10 +44,12 @@ export const ExpenseProvider = ({ children }) => {
   const expenseAmounts = transactions
     .filter((transaction) => transaction.category === "expense")
     .map((transaction) => transaction.amount);
+
   const expense =
     expenseAmounts.length > 0
       ? expenseAmounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
       : "0.00";
+
   //calculate total
   const total = Number(income - expense).toFixed(2);
 
